d6b925eee27de6051c0f24971c9c40fa8b834775
alef321 <alefMac@github.com>
2021-05-09 22:44:30 -0300

    finalizando menu (no-responsive)

---
+++ onmidia/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './componentes/Header'
+import './App.css'
+
+
+export default function App(){
+    
+    return(
+
+        <>
+           <Header/> 
+        </>
+
+    ) 
+
+}
---
+++ onmidia/src/componentes/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Icon from './icons/icon.png'
+import Svg from './svg/arrow-right-solid.svg'
+import Pontos from './imgs/pontos.png'
+
+export default function Header(){
+
+    return(
+        <>
+            <header className='menu-main'>
+                <div className='menu'>
+                    <img src={Icon} className='img-logo'/>
+                    <nav className='nav-menu'>
+                        <ul>
+                            <li className='active'><a href='#'>Inicio</a></li>
+                            <li><a href='#'>Ajuda</a></li>
+                            <li><a href='#'>Recursos</a></li>
+                            <li><a href='#'>Acessar</a></li>
+                        </ul>
+                    </nav> 
+                    <a href='#' className='btn-login'>Login <img src={Svg} className='svg-inline--fa '/></a> 
+                </div>
+            </header>
+            <img src={Pontos} className='pontos'/>
+        </>
+    )
+}
