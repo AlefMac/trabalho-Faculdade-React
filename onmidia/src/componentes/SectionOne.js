bd266761f79fddf204eb075b4008366b514a65f5
alef321 <alefMac@github.com>
2021-05-10 18:46:52 -0300

    !

--- onmidia/src/componentes/SectionOne.js
+++ onmidia/src/componentes/SectionOne.js
@@ -9,7 +9,7 @@ export default function SectionOne(){
             <section className='One'>
                 <div className='texts'>
                     <h1>Auxiliando no Ensino<br/> da Educação </h1>
-                    <p><strong>OnMidia</strong> é uma ferramenta educativa que irá<br/> disponibilizar ao seus filhos ou alunos,<br/> conteúdos didáticos de acordo com o perfil<br/> educacional e idade. Tudo com praticidade e<br/> flexibilidade usando o próprio <strong>WhatsApp</strong></p>
+                    <p><strong>OnMidia</strong> é uma ferramenta educativa que irá<br/> disponibilizar ao seus filhos ou alunos,<br/> conteúdos didáticos de acordo com o perfil<br/> educacional e idade. Tudo com praticidade e<br/> flexibilidade usando o próprio <strong>WhatsApp.</strong></p>
                 </div>
                 <img src={Img} className='Ola'/>
             </section>
